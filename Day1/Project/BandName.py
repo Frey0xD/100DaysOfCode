501d062665e87257ed9811db4338b6bc7432bf94
Frey <frey0xd@gmail.com>
2022-10-15 04:49:25 +0530

    Day1

---
+++ Day1/Project/BandName.py
@@ -0,0 +1,9 @@
+print("Welcome to the band name generator by frey")
+city = input("What is the name of the city you grew up\n")
+pet = input("What is the name of your pet\n")
+
+
+# Now the band name 
+
+
+print("Your band name is :-" + city + pet)
