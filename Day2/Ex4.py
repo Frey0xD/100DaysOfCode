a4d32d03e7d2137ffc5b1eaac5d220c8b02be4fe
Frey <frey0xd@gmail.com>
2022-10-16 21:25:38 +0530

    Day 3

--- Day2/Ex4.py
+++ Day2/Ex4.py
@@ -1,6 +1,7 @@
 #How many Days weeks months years we got left 
 
-print(                  """Hello there ! i'm a program which tells you how many Days, weeks, months, years you got left
+print(                  """Hello there ! i'm a program made by Frey
+                            which tells you how many Days, weeks, months, years you got left
                             Some notes :- Enter your Name and current age :) 
                             and i will tell you for how many Days, Weeks, Months
                             you are going to live until you turn 70""")
@@ -13,13 +14,13 @@ current_age = (int(input("Enter your current age :- ")))
 #months = 12
 
 
-years_remaining = 80 - current_age
+years_remaining = 70 - current_age
 
 days_left = years_remaining * 365
 weeks_left = years_remaining * 52
 months_left = years_remaining * 12
 print(f"Hello there {name}")
-print(f"The number of days you left until you turn 90 is {days_left}")
-print(f"The number of Weeks you left until you turn 90 is {weeks_left}")
-print(f"The number of months you left until you turn 90 is {months_left}")
-print(f"The number of years you left until you turn 90 is {years_remaining}")
+print(f"The number of days you left until you turn 70 is {days_left}")
+print(f"The number of Weeks you left until you turn 70 is {weeks_left}")
+print(f"The number of months you left until you turn 70 is {months_left}")
+print(f"The number of years you left until you turn 70 is {years_remaining}")
---
+++ Day3/odd_or_even.py
@@ -0,0 +1,6 @@
+number = (int(input("Enter any integer :- ")))
+
+if (number % 2) == 0:
+    print("it's a even number")
+else:
+    print("odd")
