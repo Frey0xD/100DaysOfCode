9229cb37f4bd1bab4451a1c7ce217cde05925300
Frey <frey0xd@gmail.com>
2022-10-16 18:07:18 +0530

    wow i lived

---
+++ Day2/Ex3.py
@@ -0,0 +1,26 @@
+# Type conservation
+##################################DATA#################################################
+ldl = 1.23
+name = "taski"
+age = 32
+alive = True
+######################################################################################
+print(int(22/7))
+print(22/7)
+print(round(22/7))
+print(round(22/7,2))
+
+result = 4/2
+result /= 2
+print(int(result))
+
+### Helps in game 
+
+score = 0
+
+score += 1
+print(score)
+print("Your score is" + str(score))
+##########################################################################################
+##########################################################################################
+print(f"Your name is {name} and your score is {score} lol Alive_Status {alive}")
---
+++ Day2/Ex4.py
@@ -0,0 +1,25 @@
+#How many Days weeks months years we got left 
+
+print(                  """Hello there ! i'm a program which tells you how many Days, weeks, months, years you got left
+                            Some notes :- Enter your Name and current age :) 
+                            and i will tell you for how many Days, Weeks, Months
+                            you are going to live until you turn 70""")
+name = (str(input("Enter your name :- ")))
+current_age = (int(input("Enter your current age :- ")))
+# if that user live up to 90 
+
+#days = 365
+#weeks = 52
+#months = 12
+
+
+years_remaining = 80 - current_age
+
+days_left = years_remaining * 365
+weeks_left = years_remaining * 52
+months_left = years_remaining * 12
+print(f"Hello there {name}")
+print(f"The number of days you left until you turn 90 is {days_left}")
+print(f"The number of Weeks you left until you turn 90 is {weeks_left}")
+print(f"The number of months you left until you turn 90 is {months_left}")
+print(f"The number of years you left until you turn 90 is {years_remaining}")
