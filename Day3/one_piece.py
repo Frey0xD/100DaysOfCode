618e10e4abc32d5115058d7ba15538800daf8a8d
Frey <frey0xd@gmail.com>
2022-10-17 10:38:54 +0530

    One Piece

---
+++ Day3/one_piece.py
@@ -0,0 +1,59 @@
+print('''*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     # `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/_____/[OnePiece]
+*******************************************************************************''')
+print("Welcome to the great pirate era\nin the search of OnePiece")
+print("So you can think you are the one who can find one piece ?")
+help = str(input("For Help and info type 'h' or press 's' to start the game without help:- (h/s) "))
+if help == 'h':
+    print("\n")
+    print("""           
+                Yo i'm caption rojer        
+                i will guide you to the one piece
+                Backgraound :- You are a pirate who is so new the sea, and while travalling
+                you saw a pirate crew and when you hang out with they you think it will be 
+                a great idea to stick with them for a while.
+                            crew members :-  Luffy
+                                             Zoro
+                                             Sanji
+                                             Nami
+                                             chopper
+                                             franki
+                                             brook
+
+                    This game contains a story based on one piece (Anime/Manga)
+                    The story is of the last arc where the crew is so close to one piece
+                    all theory based
+                    Enjoy!
+
+                        """)
+else:
+    print("\n")
+    print("Welcome to the One piece you are a part of the best young pirate crew ")
+
+name = str(input("Player Name :- "))
+task1 = (input(f"Welcome {name} You  are in the crew and suddenly you noticed that luffy your ship captain \nTurned into Joy boy and he asked to take the lead in the war. \nwhat will you do take the lead of ask some to help :- "))
+if task1 == "take the lead":
+    print("you lost as your level is so low ! you are dead")
+elif task1 == "ask for help":
+    print("That's a nice move now don't worry zoro will take things from now on you don't have to worry")
+
+print("Now as you are alone on the deck you found a map")
+task2 = str(input("
+
